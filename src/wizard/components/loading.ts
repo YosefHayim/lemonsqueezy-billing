991dd97a523385f27247d52baae359dc1285257c
YosefHayim <yosefisabag+03@gmail.com>
2026-03-14 22:35:45 +0200

    fix: remove trailing dots from spinner, show spinner char only

--- src/wizard/components/loading.ts
+++ src/wizard/components/loading.ts
@@ -5,8 +5,8 @@ declare const process: {
 export class LoadingAnimation {
   private interval: ReturnType<typeof setInterval> | null = null;
   private frame = 0;
-  private dots = 9;
   private chars = ['\u280B', '\u2819', '\u2839', '\u2838', '\u283C', '\u2834', '\u2826', '\u2827', '\u2807'];
+  private readonly charCount = this.chars.length;
 
   start(message: string = "Loading"): void {
     if (this.interval) {
@@ -16,10 +16,8 @@ export class LoadingAnimation {
     process.stdout.write(`\n${message} `);
 
     this.interval = setInterval(() => {
-      const char = this.chars[this.frame % this.dots];
-      const dots = '.'.repeat(this.frame % this.dots);
-      const spaces = ' '.repeat(this.dots - (this.frame % this.dots) - 1);
-      process.stdout.write(`\r${message} ${char}${dots}${spaces} `);
+      const char = this.chars[this.frame % this.charCount];
+      process.stdout.write(`\r${message} ${char} `);
       this.frame++;
     }, 80);
   }
